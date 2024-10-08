cdce04ccca254e46de72c506b5688ccfb2570164
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2024-10-08 20:02:14 -0300

    Update Modelo.java

--- src/main/java/Modelo.java
+++ src/main/java/Modelo.java
@@ -2,11 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * 
- */
 public class Modelo {
     private String descricao;
     private int id;
